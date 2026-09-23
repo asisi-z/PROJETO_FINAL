258d4ea93ac7c0a1e2ceed2b649c736d30e93670
asisi-z <azizi.ribeiro@ba.estudante.senai.br>
2026-09-23 13:00:49 -0300

    Configurando os treinos

--- src/app/componentes/menu/menu.ts
+++ src/app/componentes/menu/menu.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
+  menuOpen = false;
+
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router,
   ) {}
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 
   logout(): void {
     const wantsToLogout = window.confirm('Tem certeza que deseja sair do site?');
